1a1db1bd7c80413deb1df2420d965684adc09813
Álvaro Sánchez <39579936+genteinvencible@users.noreply.github.com>
2026-02-09 10:07:21 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { ThemeProvider } from './context/ThemeContext';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import AboutSection from './components/AboutSection';
-import ThemeToggle from './components/ThemeToggle';
 
 function App() {
   return (
     <ThemeProvider>
       <div className="relative">
-        <ThemeToggle />
         <Navbar />
         <Hero />
         <AboutSection />
