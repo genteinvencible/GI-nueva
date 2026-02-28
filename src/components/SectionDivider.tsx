6e51472641e0b22da9e21675e47de76b158b977e
Álvaro Sánchez <39579936+genteinvencible@users.noreply.github.com>
2026-02-28 09:31:32 +0100

    Updated BodaPage.tsx

--- src/components/SectionDivider.tsx
+++
@@ -1,7 +0,0 @@
-export default function SectionDivider() {
-  return (
-    <div className="flex justify-center items-center py-10 lg:py-14">
-      <div className="w-12 h-px bg-neutral-300 dark:bg-neutral-600 transition-colors" />
-    </div>
-  );
-}
